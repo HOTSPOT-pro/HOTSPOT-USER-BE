fa2644336a858eb1d33ee333896c43922257fd41
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-17 14:20:00 +0900

    [HOTSPOT-96] fix: Subscription Response에 id 추가

--- src/main/java/hotspot/user/subscription/controller/response/SubscriptionResponse.java
+++ src/main/java/hotspot/user/subscription/controller/response/SubscriptionResponse.java
@@ -5,12 +5,14 @@ import hotspot.user.plan.domain.mapper.PlanMapper;
 import hotspot.user.subscription.domain.Subscription;
 
 public record SubscriptionResponse(
+        Long id,
         String phone,
         PlanResponse plan,
         boolean isLocked
 ) {
     public static SubscriptionResponse from(Subscription subscription) {
         return new SubscriptionResponse(
+                subscription.getId(),
                 subscription.getPhoneEnc(),
                 PlanMapper.toPlanResponse(subscription.getPlan()),
                 subscription.getIsLocked()
