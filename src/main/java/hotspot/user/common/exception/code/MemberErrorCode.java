ce32d7949203f72e4e4f14b3208a310af5156508
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-18 21:37:41 +0900

    [HOTSPOT-96] feat: MemberErrorCode familySub 관련 에러 코드 추가

--- src/main/java/hotspot/user/common/exception/code/MemberErrorCode.java
+++ src/main/java/hotspot/user/common/exception/code/MemberErrorCode.java
@@ -14,6 +14,7 @@ public enum MemberErrorCode implements BaseErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "회원 정보를 찾을 수 없습니다."),
     SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_002", "해당 전화번호로 가입된 회선을 찾을 수 없습니다."),
     MEMBER_ALREADY_EXISTS(HttpStatus.CONFLICT, "MEMBER_003", "이미 가입된 회원입니다."),
+    FAMILY_SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_004", "회선의 가족 결합 정보를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
