ae89b0f6fa9d611b462e5ef16d610a587d89cf7d
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-21 00:48:46 +0900

    [HOTSPOT-100] fix: UpdateDataLimitRequest dataLimit에 @Min 어노테이션 추가

--- src/main/java/hotspot/user/family/controller/request/UpdateDataLimitRequest.java
+++ src/main/java/hotspot/user/family/controller/request/UpdateDataLimitRequest.java
@@ -1,5 +1,7 @@
 package hotspot.user.family.controller.request;
 
+import jakarta.validation.constraints.Min;
+
 /**
  * 구성원 데이터 한도 업데이트 request dto
  * @param familyId
@@ -9,6 +11,7 @@ package hotspot.user.family.controller.request;
 public record UpdateDataLimitRequest(
         Long familyId,
         Long subId,
+        @Min(value = -1, message = "데이터 한도는 -1(무한대) 이상이어야 합니다.")
         int dataLimit
 ) {
 }
