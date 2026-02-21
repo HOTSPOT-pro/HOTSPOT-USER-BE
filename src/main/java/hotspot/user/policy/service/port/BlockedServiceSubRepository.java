eea6fb53f2c0776d4a870dd953b322cf4b4be66f
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-21 16:27:24 +0900

    [HOTSPOT-100] fix: spotlessApply 적용

--- src/main/java/hotspot/user/policy/service/port/BlockedServiceSubRepository.java
+++ src/main/java/hotspot/user/policy/service/port/BlockedServiceSubRepository.java
@@ -1,13 +1,6 @@
 package hotspot.user.policy.service.port;
 
 import java.util.List;
-
-import hotspot.user.policy.domain.BlockedServiceSub;
-
-/**
- * 앱 차단 서비스 - 회선 매핑 테이블 리포지토리 (컨트롤러 - 서비스 구간)
- */
-import java.util.List;
 import java.util.Set;
 
 import hotspot.user.policy.domain.BlockedServiceSub;
