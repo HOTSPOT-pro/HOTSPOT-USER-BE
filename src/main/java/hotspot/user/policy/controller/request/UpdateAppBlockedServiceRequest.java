e1f9ef5fc518b5aef12acee97ca794dc2229eeeb
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-21 15:24:03 +0900

    [HOTSPOT-100] fix: AppBlockedServiceRequest dto 파라미터 변경

--- src/main/java/hotspot/user/policy/controller/request/UpdateAppBlockedServiceRequest.java
+++ src/main/java/hotspot/user/policy/controller/request/UpdateAppBlockedServiceRequest.java
@@ -6,11 +6,11 @@ import java.util.List;
  * 구성원별 차단 서비스 업데이트 request dto
  * @param familyId
  * @param subId
- * @param blockedServiceStatusList
+ * @param blockedServiceIdList
  */
 public record UpdateAppBlockedServiceRequest(
         Long familyId,
         Long subId,
-        List<AppBlockedServiceStatus> blockedServiceStatusList
+        List<Long> blockedServiceIdList
 ) {
 }
