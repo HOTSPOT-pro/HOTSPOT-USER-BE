728d21f84ea90b16a446b5da9466a0e5a6a834fc
andrew <leegd120@gmail.com>
2026-02-23 00:09:50 +0900

    fix : 들여쓰기 4칸으로 통일

--- src/main/java/hotspot/user/common/config/SecurityConfig.java
+++ src/main/java/hotspot/user/common/config/SecurityConfig.java
@@ -20,43 +20,43 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-  private final JwtFilter jwtFilter;
-  private final CustomOidcUserService customOidcUserService;
-  private final OAuth2SuccessHandler oAuth2SuccessHandler;
-  private final OAuth2FailureHandler oAuth2FailureHandler;
-  private final CorsConfig corsConfig;
+    private final JwtFilter jwtFilter;
+    private final CustomOidcUserService customOidcUserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+    private final CorsConfig corsConfig;
 
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http
-      .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
-      .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
-      .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 방식 비활성화
-      .formLogin(FormLoginConfigurer::disable) // 기본 로그인 비활성화
-      .httpBasic(AbstractHttpConfigurer::disable); // Basic 인증 비활성화 : Basic 인증은 사용자 이름 & 비밀번호를 Base64로 인코딩하여 인증값으로 활용
+        http
+                .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 방식 비활성화
+                .formLogin(FormLoginConfigurer::disable) // 기본 로그인 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable); // Basic 인증 비활성화 : Basic 인증은 사용자 이름 & 비밀번호를 Base64로 인코딩하여 인증값으로 활용
 
-    // UsernamePasswordAuthenticationFilter : 이 클래스에서 폼 로그인 인증을 처리
-    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        // UsernamePasswordAuthenticationFilter : 이 클래스에서 폼 로그인 인증을 처리
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-    http.oauth2Login(oauth -> oauth
-                    .userInfoEndpoint(userInfo -> userInfo.oidcUserService(customOidcUserService))
-                    .successHandler(oAuth2SuccessHandler)
-                    .failureHandler(oAuth2FailureHandler));
-
-
-    http.authorizeHttpRequests(auth -> auth
-        .requestMatchers(
-            "/", "/health", "/login/**", "/oauth2/**", "/oauth/**",
-            "/swagger-ui/**", "/v3/api-docs/**", "/actuator/health",
-            "/api/v1/auth/onboarding",
-            "/api/v1/**" // 테스트 위해서 모든 API 열어둠 [To-Do] 삭제 필요
-        ).permitAll()
-        .anyRequest().authenticated()
-    );
+        http.oauth2Login(oauth -> oauth
+                .userInfoEndpoint(userInfo -> userInfo.oidcUserService(customOidcUserService))
+                .successHandler(oAuth2SuccessHandler)
+                .failureHandler(oAuth2FailureHandler));
 
 
-    return http.build();
-  }
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/", "/health", "/login/**", "/oauth2/**", "/oauth/**",
+                        "/swagger-ui/**", "/v3/api-docs/**", "/actuator/health",
+                        "/api/v1/auth/onboarding",
+                        "/api/v1/**" // 테스트 위해서 모든 API 열어둠 [To-Do] 삭제 필요
+                ).permitAll()
+                .anyRequest().authenticated()
+        );
+
+
+        return http.build();
+    }
 }
