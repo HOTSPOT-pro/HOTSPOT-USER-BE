498cb90718d76f001d5b47d8464e3692d1d6db1e
Jae <lijae3552@gmail.com>
2026-02-23 17:04:35 +0900

    [HOTSPOT-150] feat: 알림 조회/개수/읽음처리 서비스 포트 정의

---
+++ src/main/java/hotspot/user/notification/controller/port/NotificationService.java
@@ -0,0 +1,18 @@
+package hotspot.user.notification.controller.port;
+
+import org.springframework.data.domain.Pageable;
+
+import hotspot.user.notification.controller.response.NotificationListResponse;
+import hotspot.user.notification.controller.response.UnreadNotificationCountResponse;
+
+public interface NotificationService {
+
+    // 인증 사용자 기준으로 최근 알림 목록을 반환한다.
+    NotificationListResponse findNotifications(Long memberId, Pageable pageable);
+
+    // 인증 사용자 기준으로 안 읽은 알림 수를 반환한다.
+    UnreadNotificationCountResponse findUnreadCount(Long memberId);
+
+    // 인증 사용자 기준으로 모든 알림을 읽음 처리한다.
+    void markAllRead(Long memberId);
+}
