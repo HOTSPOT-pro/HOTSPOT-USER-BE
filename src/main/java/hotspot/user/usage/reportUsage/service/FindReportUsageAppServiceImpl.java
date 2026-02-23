ec9a6023a2865a64ab094adf445771cd1a3c5b94
andrew <leegd120@gmail.com>
2026-02-23 18:40:05 +0900

    feat : ReportUsage Service 계층 구현

---
+++ src/main/java/hotspot/user/usage/reportUsage/service/FindReportUsageAppServiceImpl.java
@@ -0,0 +1,87 @@
+package hotspot.user.usage.reportUsage.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import hotspot.user.policy.domain.AppBlockedService;
+import hotspot.user.policy.service.port.AppBlockedServiceRepository;
+import hotspot.user.subscription.domain.Subscription;
+import hotspot.user.subscription.service.SubscriptionService;
+import hotspot.user.usage.reportUsage.controller.port.FindReportUsageAppDayService;
+import hotspot.user.usage.reportUsage.controller.port.FindReportUsageAppMonthService;
+import hotspot.user.usage.reportUsage.controller.response.ReportUsageAppResponse;
+import hotspot.user.usage.reportUsage.domain.AppUsage;
+import hotspot.user.usage.reportUsage.domain.mapper.ReportUsageAppMapper;
+import hotspot.user.usage.reportUsage.service.port.ReportUsageAppRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FindReportUsageAppServiceImpl
+        implements FindReportUsageAppMonthService, FindReportUsageAppDayService {
+
+    private final SubscriptionService subscriptionService;
+    private final ReportUsageAppRepository reportUsageAppRepository;
+    private final AppBlockedServiceRepository appBlockedServiceRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public ReportUsageAppResponse findReportUsageAppMonth(Long memberId) {
+
+        Subscription subscription =
+                subscriptionService.findByMemberId(memberId);
+
+        // Redis 조회
+        List<AppUsage> appUsages =
+                reportUsageAppRepository
+                        .findMonthlyAppUsage(subscription.getId());
+
+        // appId 목록 추출
+        List<Long> appIds =
+                appUsages.stream()
+                        .map(AppUsage::getAppId)
+                        .toList();
+
+        // DB 조회
+        List<AppBlockedService> services =
+                appBlockedServiceRepository
+                        .findAllByAppBlackedServiceIds(appIds);
+
+        // Mapper 위임
+        return ReportUsageAppMapper.toReportUsageAppResponse(
+                LocalDateTime.now(),
+                appUsages,
+                ReportUsageAppMapper.toAppIdNameMap(services)
+        );
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public ReportUsageAppResponse findReportUsageAppDay(Long memberId) {
+
+        Subscription subscription =
+                subscriptionService.findByMemberId(memberId);
+
+        List<AppUsage> appUsages =
+                reportUsageAppRepository
+                        .findDailyAppUsage(subscription.getId());
+
+        List<Long> appIds =
+                appUsages.stream()
+                        .map(AppUsage::getAppId)
+                        .toList();
+
+        List<AppBlockedService> services =
+                appBlockedServiceRepository
+                        .findAllByAppBlackedServiceIds(appIds);
+
+        return ReportUsageAppMapper.toReportUsageAppResponse(
+                LocalDateTime.now(),
+                appUsages,
+                ReportUsageAppMapper.toAppIdNameMap(services)
+        );
+    }
+}
