28733a4a222c7f4b42c00a9cb59c5c47e313b6f0
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-23 14:49:50 +0900

    [HOTSPOT-144] fIx: 가족 전체 정보 조회 response & mapper 수정

--- src/main/java/hotspot/user/family/controller/response/FamilyInfoResponse.java
+++ src/main/java/hotspot/user/family/controller/response/FamilyInfoResponse.java
@@ -1,16 +1,17 @@
 package hotspot.user.family.controller.response;
 
 import hotspot.user.member.controller.response.MemberResponse;
+import lombok.Builder;
 
 import java.util.List;
 
 /**
  * 가족 정보 조회 응답 Dto
  */
+@Builder
 public record FamilyInfoResponse(
         Long familyId,
         int familyNum,
-        int familyDataAmount,
         List<MemberResponse> memberInfoList
 ) {
 }
