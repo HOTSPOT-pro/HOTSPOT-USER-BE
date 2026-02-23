556d4812d2a0368ef466d17e2339871a998b84e7
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-23 11:45:06 +0900

    [HOTSPOT-143] feat: 구성원 정보 조회 Controller & Swagger 작성

--- src/main/java/hotspot/user/member/controller/MemberController.java
+++ src/main/java/hotspot/user/member/controller/MemberController.java
@@ -10,17 +10,19 @@ import hotspot.user.common.ApiResponse;
 import hotspot.user.common.security.PrincipalDetails;
 import hotspot.user.member.controller.port.FindMemberService;
 import hotspot.user.member.controller.response.MemberResponse;
+import hotspot.user.member.controller.swagger.MemberApi;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
-public class MemberController {
+public class MemberController implements MemberApi {
 
     private final FindMemberService findMemberService;
 
     // 내 정보 조회
     // [To-Do] 구성원 별 정보 조회는 전체 가족 조회할 때 모두 포함되는 값이라서 따로 API로 만들진 않았는데 추후 필요하면 추가하기
+    @Override
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<MemberResponse>> getMemberInfo(
             @AuthenticationPrincipal PrincipalDetails principal) {
