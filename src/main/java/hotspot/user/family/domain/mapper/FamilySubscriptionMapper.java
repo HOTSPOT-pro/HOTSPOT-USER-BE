66a061f5a4495aad596f18f6fb33ed6d71fcabcf
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-24 01:16:33 +0900

    [HOTSPOT-146] feat: FamilySubscription -> UpdateFamilyRoleResponse Mapper 추가

--- src/main/java/hotspot/user/family/domain/mapper/FamilySubscriptionMapper.java
+++ src/main/java/hotspot/user/family/domain/mapper/FamilySubscriptionMapper.java
@@ -1,6 +1,8 @@
 package hotspot.user.family.domain.mapper;
 
+import hotspot.user.family.controller.request.UpdateFamilyRoleRequest;
 import hotspot.user.family.controller.response.UpdateDataLimitResponse;
+import hotspot.user.family.controller.response.UpdateFamilyRoleResponse;
 import hotspot.user.family.domain.FamilySubscription;
 
 /**
@@ -11,12 +13,21 @@ import hotspot.user.family.domain.FamilySubscription;
 public class FamilySubscriptionMapper {
     // request -> domain
 
+
     // domain -> response
     public static UpdateDataLimitResponse toUpdateDataLimitResponse(FamilySubscription familySub) {
         return UpdateDataLimitResponse.builder()
                 .familyId(familySub.getFamily().getId())
                 .subId(familySub.getSubscription().getId())
                 .dataLimit(familySub.getDataLimit())
+                .build();
+    }
+
+    public static UpdateFamilyRoleResponse toUpdateFamilyRoleResponse(FamilySubscription familySub) {
+        return UpdateFamilyRoleResponse.builder()
+                .familyId(familySub.getFamily().getId())
+                .subId(familySub.getSubscription().getId())
+                .familyRole(familySub.getFamilyRole())
                 .build();
     }
 }
