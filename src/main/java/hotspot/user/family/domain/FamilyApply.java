2a039ecae2fd1d3f1ce2af13368864c0d567e850
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-23 19:42:54 +0900

    [HOTSPOT-145] feat: 가족 구성원 추가 / 삭제 domain & mapper 생성

---
+++ src/main/java/hotspot/user/family/domain/FamilyApply.java
@@ -0,0 +1,23 @@
+package hotspot.user.family.domain;
+
+import hotspot.user.member.domain.FamilyRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 가족 구성원 추가 / 신청 도메인
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class FamilyApply {
+    private Long id;
+    private Long requesterSubId;
+    private Long targetSubId;
+    private Long familyId;
+    private ApplyType applyType;
+    private FamilyRole targetFamilyRole;
+    private String docUrl;
+    private ApplyStatus status;
+}
