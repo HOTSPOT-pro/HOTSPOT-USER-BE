c5b891d299b73e97394547a137cd806ccd826007
Jae <lijae3552@gmail.com>
2026-02-23 22:48:37 +0900

    [HOTSPOT-151] feat: Kafka 매핑/검증용 공통 예외 코드 추가

---
+++ src/main/java/hotspot/user/common/exception/code/KafkaErrorCode.java
@@ -0,0 +1,21 @@
+package hotspot.user.common.exception.code;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum KafkaErrorCode implements BaseErrorCode {
+    UNSUPPORTED_KAFKA_EVENT_TYPE(HttpStatus.BAD_REQUEST, "KAFKA_001", "지원하지 않는 Kafka eventType 입니다."),
+    UNSUPPORTED_KAFKA_ALERT_TYPE(HttpStatus.BAD_REQUEST, "KAFKA_002", "지원하지 않는 Kafka alertType 입니다."),
+    UNSUPPORTED_KAFKA_THRESHOLD(HttpStatus.BAD_REQUEST, "KAFKA_003", "지원하지 않는 Kafka threshold 입니다."),
+    KAFKA_EVENT_ID_REQUIRED(HttpStatus.BAD_REQUEST, "KAFKA_004", "Kafka eventId(sourceEventId or alertId)가 필요합니다."),
+    KAFKA_SUB_ID_REQUIRED(HttpStatus.BAD_REQUEST, "KAFKA_005", "Kafka subId가 필요합니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String customCode;
+    private final String message;
+}
