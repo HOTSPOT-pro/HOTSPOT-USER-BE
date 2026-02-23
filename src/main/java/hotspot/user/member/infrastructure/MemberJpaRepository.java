f4b9bb3590acdc182cd96ccd783ba8a08c85729a
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-23 11:07:48 +0900

    [HOTSPOT-143] feat: 구성원 정보 상세 조회 Repository 코드 추가

--- src/main/java/hotspot/user/member/infrastructure/MemberJpaRepository.java
+++ src/main/java/hotspot/user/member/infrastructure/MemberJpaRepository.java
@@ -1,10 +1,28 @@
 package hotspot.user.member.infrastructure;
 
+import java.util.List;
+import java.util.Optional;
+
+import hotspot.user.member.domain.MemberDetailInfoDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import hotspot.user.member.infrastructure.entity.MemberEntity;
 
+
 @Repository
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
+
+    // MemberId로 여러 테이블 JOIN 해서 한 번에 조회
+    @Query("""
+           SELECT m, sa.email, s.phoneEnc, s.subId, fs.familyRole, fs.family.familyId
+           FROM MemberEntity m
+           LEFT JOIN SocialAccountEntity sa ON sa.member = m
+           LEFT JOIN SubscriptionEntity s ON s.member = m
+           LEFT JOIN FamilySubscriptionEntity fs ON fs.subscription = s
+           WHERE m.id = :memberId
+           """)
+    Optional<MemberDetailInfoDto> findDetailQueryResult(@Param("memberId") Long memberId);
 }
--- src/main/java/hotspot/user/member/service/port/MemberRepository.java
+++ src/main/java/hotspot/user/member/service/port/MemberRepository.java
@@ -3,10 +3,12 @@ package hotspot.user.member.service.port;
 import java.util.Optional;
 
 import hotspot.user.member.domain.Member;
+import hotspot.user.member.domain.MemberDetailInfo;
 
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
+    Optional<MemberDetailInfo> findDetailById(Long id); // 멤버 상세 정보 조회할 떄 JOIN으로 가져오기
     void delete(Member member);
     void deleteById(Long id);
 }
