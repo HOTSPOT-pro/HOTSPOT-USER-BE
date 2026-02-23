fdb2ac7d0fe78817d00bd17472ac4f000c5674b7
andrew <leegd120@gmail.com>
2026-02-23 18:52:30 +0900

    fix : spotlessApply 에러 해결

--- src/main/java/hotspot/user/usage/reportUsage/controller/ReportUsageController.java
+++ src/main/java/hotspot/user/usage/reportUsage/controller/ReportUsageController.java
@@ -1,6 +1,5 @@
 package hotspot.user.usage.reportUsage.controller;
 
-import hotspot.user.usage.reportUsage.controller.swagger.ReportUsageApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import hotspot.user.usage.reportUsage.controller.port.FindReportUsageMonthServic
 import hotspot.user.usage.reportUsage.controller.response.ReportUsageAppResponse;
 import hotspot.user.usage.reportUsage.controller.response.ReportUsageDayResponse;
 import hotspot.user.usage.reportUsage.controller.response.ReportUsageMonthResponse;
+import hotspot.user.usage.reportUsage.controller.swagger.ReportUsageApi;
 import lombok.RequiredArgsConstructor;
 
 @RestController
