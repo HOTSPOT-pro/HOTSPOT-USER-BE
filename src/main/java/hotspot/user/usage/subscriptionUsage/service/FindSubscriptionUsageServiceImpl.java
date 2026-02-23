9bab30019d6f9818d9b0c5f18e8c971c86d4aed1
andrew <leegd120@gmail.com>
2026-02-23 11:25:55 +0900

    feat : 회선 개인 데이터 사용량 조회 Service 구현

---
+++ src/main/java/hotspot/user/usage/subscriptionUsage/service/FindSubscriptionUsageServiceImpl.java
@@ -0,0 +1,50 @@
+package hotspot.user.usage.subscriptionUsage.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import hotspot.user.presentData.service.port.PresentDataRepository;
+import hotspot.user.usage.subscriptionUsage.controller.port.FindSubscriptionUsageService;
+import hotspot.user.usage.subscriptionUsage.controller.response.SubscriptionUsageResponse;
+import hotspot.user.usage.subscriptionUsage.domain.GiftUsage;
+import hotspot.user.usage.subscriptionUsage.domain.SubscriptionUsage;
+import hotspot.user.usage.subscriptionUsage.domain.mapper.SubscriptionUsageMapper;
+import hotspot.user.usage.subscriptionUsage.service.port.SubscriptionUsageRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FindSubscriptionUsageServiceImpl implements FindSubscriptionUsageService {
+
+    private final SubscriptionUsageRepository subscriptionUsageRepository;
+    private final PresentDataRepository presentDataRepository;
+
+    @Override
+    public SubscriptionUsageResponse findSubscriptionUsage(Long subscriptionId) {
+
+        // Redis에서 숫자 + giftId 조회
+        SubscriptionUsage usage =
+                subscriptionUsageRepository
+                        .findSubscriptionUsage(subscriptionId);
+
+        // giftId 리스트 추출
+        List<Long> giftIds =
+                usage.gifts()
+                        .stream()
+                        .map(GiftUsage::giftId)
+                        .toList();
+
+        // giftId → username 매핑
+        Map<Long, String> giftIdToUserName =
+                presentDataRepository.findGiftGiverNames(giftIds);
+
+        // Mapper에 전달
+        return SubscriptionUsageMapper
+                .toSubscriptionUsageResponse(
+                        usage,
+                        giftIdToUserName
+                );
+    }
+}
