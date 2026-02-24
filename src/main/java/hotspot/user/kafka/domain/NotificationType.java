01bbc81307b06499fb7d290470a269bcb5bd27a2
Jae <lijae3552@gmail.com>
2026-02-25 01:38:13 +0900

    [HOTSPOT-160] feat: NotificationCategory 매핑 책임 추가

--- src/main/java/hotspot/user/kafka/domain/NotificationType.java
+++ src/main/java/hotspot/user/kafka/domain/NotificationType.java
@@ -1,23 +1,35 @@
 package hotspot.user.kafka.domain;
 
+import hotspot.user.notification.domain.NotificationCategory;
+
 public enum NotificationType {
-    SINGLE_USAGE_THRESHOLD_50,
-    SINGLE_USAGE_THRESHOLD_30,
-    SINGLE_USAGE_THRESHOLD_10,
-    SINGLE_USAGE_EXHAUSTED,
-    FAMILY_USAGE_THRESHOLD_50,
-    FAMILY_USAGE_THRESHOLD_30,
-    FAMILY_USAGE_THRESHOLD_10,
-    FAMILY_USAGE_EXHAUSTED,
-    PRESENT_USAGE_THRESHOLD_50,
-    PRESENT_USAGE_THRESHOLD_30,
-    PRESENT_USAGE_THRESHOLD_10,
-    PRESENT_USAGE_EXHAUSTED,
-    TIME_WINDOW_POLICY_APPLIED,
-    TIME_WINDOW_POLICY_RELEASED,
-    IMMEDIATE_BLOCK_APPLIED,
-    IMMEDIATE_BLOCK_RELEASED,
-    SERVICE_ACCESS_BLOCKED,
-    SERVICE_ACCESS_RELEASED,
-    PRESENT_DATA
+    SINGLE_USAGE_THRESHOLD_50(NotificationCategory.DATA),
+    SINGLE_USAGE_THRESHOLD_30(NotificationCategory.DATA),
+    SINGLE_USAGE_THRESHOLD_10(NotificationCategory.DATA),
+    SINGLE_USAGE_EXHAUSTED(NotificationCategory.DATA),
+    FAMILY_USAGE_THRESHOLD_50(NotificationCategory.DATA),
+    FAMILY_USAGE_THRESHOLD_30(NotificationCategory.DATA),
+    FAMILY_USAGE_THRESHOLD_10(NotificationCategory.DATA),
+    FAMILY_USAGE_EXHAUSTED(NotificationCategory.DATA),
+    PRESENT_USAGE_THRESHOLD_50(NotificationCategory.PRESENT),
+    PRESENT_USAGE_THRESHOLD_30(NotificationCategory.PRESENT),
+    PRESENT_USAGE_THRESHOLD_10(NotificationCategory.PRESENT),
+    PRESENT_USAGE_EXHAUSTED(NotificationCategory.PRESENT),
+    TIME_WINDOW_POLICY_APPLIED(NotificationCategory.POLICY),
+    TIME_WINDOW_POLICY_RELEASED(NotificationCategory.POLICY),
+    IMMEDIATE_BLOCK_APPLIED(NotificationCategory.POLICY),
+    IMMEDIATE_BLOCK_RELEASED(NotificationCategory.POLICY),
+    SERVICE_ACCESS_BLOCKED(NotificationCategory.APP_SERVICE),
+    SERVICE_ACCESS_RELEASED(NotificationCategory.APP_SERVICE),
+    PRESENT_DATA(NotificationCategory.PRESENT);
+
+    private final NotificationCategory category;
+
+    NotificationType(NotificationCategory category) {
+        this.category = category;
+    }
+
+    public NotificationCategory category() {
+        return category;
+    }
 }
