01ffdcb2085e5e001e92187abe832983d7a88a42
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-24 16:36:56 +0900

    [HOTSPOT-159] feat: SocialAccountJpaRepository soft delete 적용

--- src/main/java/hotspot/user/member/infrastructure/SocialAccountJpaRepository.java
+++ src/main/java/hotspot/user/member/infrastructure/SocialAccountJpaRepository.java
@@ -3,9 +3,11 @@ package hotspot.user.member.infrastructure;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import hotspot.user.member.infrastructure.entity.SocialAccountEntity;
 
@@ -18,5 +20,9 @@ public interface SocialAccountJpaRepository extends JpaRepository<SocialAccountE
 
     Optional<SocialAccountEntity> findByMemberId(Long memberId);
 
-    void deleteByMemberId(Long memberId);
+    // Soft Delete
+    @Modifying
+    @Transactional
+    @Query("UPDATE SocialAccountEntity sa SET sa.isDeleted = true WHERE sa.member.id = :memberId")
+    void deleteByMemberId(@Param("memberId") Long memberId);
 }
