233b39504c9b2e547d48c630f9bcb7f019885750
andrew <leegd120@gmail.com>
2026-02-24 11:03:00 +0900

    feat : 가족 데이터 구성원 데이터 현황 조회 Controller 구현

--- src/main/java/hotspot/user/presentData/controller/PresentDataController.java
+++ src/main/java/hotspot/user/presentData/controller/PresentDataController.java
@@ -1,6 +1,12 @@
 package hotspot.user.presentData.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+ import hotspot.user.common.ApiResponse;
+ import hotspot.user.common.security.PrincipalDetails;
+ import hotspot.user.presentData.controller.port.FindFamilyDataService;import hotspot.user.presentData.controller.response.FamilyDataResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -10,5 +16,13 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/presentData")
 public class PresentDataController {
 
+    private final FindFamilyDataService findFamilyDataService;
 
+    @GetMapping
+    public ResponseEntity<ApiResponse<FamilyDataResponse>> findFamilyData(
+            @AuthenticationPrincipal PrincipalDetails details) {
+        return ResponseEntity.ok(ApiResponse.success(
+                findFamilyDataService.findFamilyData(
+                        details.getId(), details.getFamilyId())));
+    }
 }
