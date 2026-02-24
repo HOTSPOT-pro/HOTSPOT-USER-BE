3edcc8fd9fa7dae577a2a135e1adba224723f00a
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-24 16:59:53 +0900

    [HOTSPOT-159] refactor: 로그아웃 리팩토링

--- src/main/java/hotspot/user/auth/controller/port/LogoutService.java
+++ src/main/java/hotspot/user/auth/controller/port/LogoutService.java
@@ -3,8 +3,13 @@ package hotspot.user.auth.controller.port;
 import hotspot.user.auth.controller.request.TokenRequest;
 
 /**
- * 로그아웃
+ * 로그아웃 서비스 포트
  */
 public interface LogoutService {
-    void logout(TokenRequest request);
+    /**
+     * 로그아웃을 처리합니다. (토큰 삭제)
+     * @param memberId 현재 로그인한 유저의 ID
+     * @param request Refresh Token 정보를 담은 객체
+     */
+    void logout(Long memberId, TokenRequest request);
 }
