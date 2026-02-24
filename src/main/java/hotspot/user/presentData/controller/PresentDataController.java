646356ff918e8adeae50f49977a9df6131c78f17
andrew <leegd120@gmail.com>
2026-02-24 14:56:46 +0900

    feat : 선물 조회 Controller 계층 구현

--- src/main/java/hotspot/user/presentData/controller/PresentDataController.java
+++ src/main/java/hotspot/user/presentData/controller/PresentDataController.java
@@ -3,10 +3,14 @@ package hotspot.user.presentData.controller;
  import hotspot.user.common.ApiResponse;
  import hotspot.user.common.security.PrincipalDetails;
  import hotspot.user.presentData.controller.port.FindFamilyDataService;
+ import hotspot.user.presentData.controller.port.FindPresentReceiveService;
+ import hotspot.user.presentData.controller.port.FindPresentProvideService;
  import hotspot.user.presentData.controller.response.FamilyDataResponse;
+ import hotspot.user.presentData.controller.response.PresentDataResponse;
  import org.springframework.http.ResponseEntity;
  import org.springframework.security.core.annotation.AuthenticationPrincipal;
  import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PathVariable;
  import org.springframework.web.bind.annotation.RequestMapping;
  import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +22,29 @@ import lombok.RequiredArgsConstructor;
 public class PresentDataController {
 
     private final FindFamilyDataService findFamilyDataService;
-
+    private final FindPresentReceiveService findPresentReceiveService;
+    private final FindPresentProvideService findPresentProvideService;
     @GetMapping
     public ResponseEntity<ApiResponse<FamilyDataResponse>> findFamilyData(
             @AuthenticationPrincipal PrincipalDetails details) {
         return ResponseEntity.ok(ApiResponse.success(
                 findFamilyDataService.findFamilyData(
                         details.getId(), details.getFamilyId())));
+    }
+
+    @GetMapping("/receive/{memberId}")
+    public ResponseEntity<ApiResponse<PresentDataResponse>> findPresentReceive(
+            @PathVariable Long memberId
+    ) {
+        return ResponseEntity.ok(ApiResponse.success(
+                findPresentReceiveService.findPresentReceive(memberId)));
+    }
+
+    @GetMapping("/provide/{memberId}")
+    public ResponseEntity<ApiResponse<PresentDataResponse>> findPresentProvide(
+            @PathVariable Long memberId
+    ) {
+        return ResponseEntity.ok(ApiResponse.success(
+                findPresentProvideService.findPresentProvide(memberId)));
     }
 }
