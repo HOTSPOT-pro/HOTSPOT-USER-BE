1d5b4cd609760c5a9ec17fd24341f20722d3347e
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-25 10:19:34 +0900

    [HOTSPOT-163] refactor: BlockPolicy import 경로 추가

--- src/main/java/hotspot/user/policy/controller/swagger/BlockPolicyApi.java
+++ src/main/java/hotspot/user/policy/controller/swagger/BlockPolicyApi.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
-import hotspot.user.common.ApiResponse;
 import hotspot.user.common.exception.ErrorResponse;
 import hotspot.user.policy.controller.response.BlockPolicyResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -18,14 +18,18 @@ public interface BlockPolicyApi {
 
     @Operation(summary = "전체 정책 목록 조회", description = "시스템에 정의된 모든 차단 정책(앱 차단, 시간 제한 등) 목록을 조회합니다.")
     @ApiResponses(value = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "조회 성공"),
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 실패\n"
-                                     + "- AUTH_001: 유효하지 않은 토큰\n"
-                                     + "- AUTH_002: 만료된 토큰",
+        @ApiResponse(responseCode = "200", description = "조회 성공"),
+        @ApiResponse(responseCode = "401", description = """
+                인증 실패
+                - AUTH_001: 유효하지 않은 토큰
+                - AUTH_002: 만료된 토큰
+                """,
                  content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "권한 없음\n"
-                                     + "- AUTH_004: 온보딩 미완료 유저(PENDING)",
+        @ApiResponse(responseCode = "403", description = """
+                권한 없음
+                - AUTH_004: 온보딩 미완료 유저(PENDING)
+                """,
                  content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    ResponseEntity<ApiResponse<List<BlockPolicyResponse>>> getAllPolicies();
+    ResponseEntity<hotspot.user.common.ApiResponse<List<BlockPolicyResponse>>> getAllPolicies();
 }
