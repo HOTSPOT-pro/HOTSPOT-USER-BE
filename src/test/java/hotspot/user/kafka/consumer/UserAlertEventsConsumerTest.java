f20b8a119ba9e93a92b6c21e903927d21c28e320
Jae <lijae3552@gmail.com>
2026-02-25 13:34:18 +0900

    [HOTSPOT-165] test: 오케스트레이터 매퍼 분리 구조 반영 및 소비자 테스트 정리

--- src/test/java/hotspot/user/kafka/consumer/UserAlertEventsConsumerTest.java
+++ src/test/java/hotspot/user/kafka/consumer/UserAlertEventsConsumerTest.java
@@ -31,7 +31,7 @@ import hotspot.user.family.domain.FamilySubscription;
 import hotspot.user.family.service.port.FamilySubscriptionRepository;
 import hotspot.user.kafka.dto.UserAlertEvent;
 import hotspot.user.kafka.dto.UserAlertNotificationsPersistedEvent;
-import hotspot.user.kafka.mapper.UserAlertEventNotificationMapper;
+import hotspot.user.kafka.mapper.orchestrator.UserAlertEventNotificationMapper;
 import hotspot.user.notification.domain.Notification;
 import hotspot.user.notification.domain.NotificationAllow;
 import hotspot.user.notification.domain.NotificationCategory;
