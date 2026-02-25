e696e929cbbea5e0d446f177b94f0c2fd2f447a4
Jae <lijae3552@gmail.com>
2026-02-26 01:09:19 +0900

    [HOTSPOT-166] feat: 저장 및 발행 실패 에러 코드 추가

--- src/main/java/hotspot/user/common/exception/code/OutboxErrorCode.java
+++ src/main/java/hotspot/user/common/exception/code/OutboxErrorCode.java
@@ -13,6 +13,16 @@ public enum OutboxErrorCode implements BaseErrorCode {
             "OUTBOX_001",
             "Outbox payload serialization failed."
     ),
+    OUTBOX_EVENT_SAVE_FAILED(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            "OUTBOX_002",
+            "Outbox event save failed."
+    ),
+    OUTBOX_EVENT_PUBLISH_FAILED(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            "OUTBOX_003",
+            "Outbox event publish failed."
+    ),
     ;
 
     private final HttpStatus httpStatus;
