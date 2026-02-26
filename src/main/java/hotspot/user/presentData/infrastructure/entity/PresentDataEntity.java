591563212a27d266edf597b2ea375b49ad5ddfbf
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-26 15:51:57 +0900

    [HOTSPOT-179] fix: PresentDataEntity domainToEntity createdTime 수정

--- src/main/java/hotspot/user/presentData/infrastructure/entity/PresentDataEntity.java
+++ src/main/java/hotspot/user/presentData/infrastructure/entity/PresentDataEntity.java
@@ -1,5 +1,7 @@
 package hotspot.user.presentData.infrastructure.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,7 +12,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-import hotspot.user.common.BaseEntity;
 import hotspot.user.presentData.domain.PresentData;
 import hotspot.user.subscription.infrastructure.entity.SubscriptionEntity;
 import lombok.AccessLevel;
@@ -18,8 +19,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
