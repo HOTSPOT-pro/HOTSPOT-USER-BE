dbab5c6112287c9d859afb72e9aa7c79040b3e28
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-26 11:53:27 +0900

    [HOTSPOT-179] feat: 데이터 선물하기 Service 생성

---
+++ src/main/java/hotspot/user/presentData/service/SendPresentDataServiceImpl.java
@@ -0,0 +1,74 @@
+package hotspot.user.presentData.service;
+
+import hotspot.user.common.exception.ApplicationException;
+import hotspot.user.common.exception.code.AuthErrorCode;
+import hotspot.user.common.exception.code.GlobalErrorCode;
+import hotspot.user.common.exception.code.MemberErrorCode;
+import hotspot.user.family.domain.FamilySubscription;
+import hotspot.user.family.service.port.FamilySubscriptionRepository;
+import hotspot.user.presentData.controller.port.SendPresentDataService;
+import hotspot.user.presentData.controller.request.SendPresentDataRequest;
+import hotspot.user.presentData.controller.response.SendPresentDataResponse;
+import hotspot.user.presentData.domain.PresentData;
+import hotspot.user.presentData.infrastructure.mapper.SendPresentDataMapper;
+import hotspot.user.presentData.service.port.PresentDataRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+/**
+ * 데이터 선물하기 서비스 구현체
+ */
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SendPresentDataServiceImpl implements SendPresentDataService {
+    private final PresentDataRepository presentDataRepository;
+    private final FamilySubscriptionRepository familySubscriptionRepository;
+
+    @Override
+    @Transactional
+    public SendPresentDataResponse sendPresentData(Long memberId, SendPresentDataRequest request) {
+
+        log.info("전달 받은 memberId: {}, 전닫ㄹ 받은 subId: {}", memberId, request.targetSubId());
+
+        // 1. 보내는 사람과 받는 사람의 가족 정보 조회
+        // 보내는 사람: memberId 기반 조회
+        FamilySubscription providerFamilySub = familySubscriptionRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new ApplicationException(MemberErrorCode.FAMILY_SUBSCRIPTION_NOT_FOUND));
+
+        // 받는 사람: subId 기반 조회
+        FamilySubscription targetFamilySub = familySubscriptionRepository.findBySubId(request.targetSubId())
+                .orElseThrow(() -> new ApplicationException(MemberErrorCode.FAMILY_SUBSCRIPTION_NOT_FOUND));
+
+        // 2. 같은 가족 구성원인지 확인
+        if (!providerFamilySub.getFamily().getId().equals(targetFamilySub.getFamily().getId())) {
+            throw new ApplicationException(AuthErrorCode.ACCESS_DENIED);
+        }
+
+        // 3. 선물 가능 단위 및 범위 확인 (1GB ~ 5GB, 1GB 단위)
+        validateDataAmount(request.dataAmount());
+
+        // [To-Do] 4. 현재 남은 데이터 양보다 더 많이 보내는지 확인
+
+        // 5. DB 기록 저장
+        PresentData presentData = SendPresentDataMapper.toPresentData(providerFamilySub.getSubscription(), targetFamilySub.getSubscription(), request.dataAmount());
+        PresentData sentPresentData = presentDataRepository.sendPresentData(presentData);
+
+        // 6. [To-Do] Redis 사용량 업데이트
+        // 주는 사람: 사용량 증가, 받는 사람: 선물 받은 데이터 양 증가?
+
+        return SendPresentDataMapper.toSendPresentDataResponse(sentPresentData);
+    }
+
+    private void validateDataAmount(Long amount) {
+        if (amount == null || amount < 1 || amount > 5) {
+            log.warn("데이터 선물 유효성 검증 실패: 요청량={}GB", amount);
+            throw new ApplicationException(GlobalErrorCode.VALIDATION_FAILED);
+        }
+    }
+}
