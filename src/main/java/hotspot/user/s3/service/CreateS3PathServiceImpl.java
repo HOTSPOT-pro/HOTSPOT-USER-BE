d5a272f749bbf87556c4433e094b36f4cfafbe3a
andrew <leegd120@gmail.com>
2026-02-26 16:36:35 +0900

    refactor : S3 Util을 활용하는 구조로 변경

--- src/main/java/hotspot/user/s3/service/CreateS3PathServiceImpl.java
+++ src/main/java/hotspot/user/s3/service/CreateS3PathServiceImpl.java
@@ -9,55 +9,52 @@ import org.springframework.stereotype.Service;
 
 import hotspot.user.common.exception.ApplicationException;
 import hotspot.user.common.exception.code.S3ErrorCode;
+import hotspot.user.common.util.s3.S3Util;
 import hotspot.user.s3.controller.port.CreateS3PathService;
 import hotspot.user.s3.controller.response.S3PathResponse;
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 @Service
 @RequiredArgsConstructor
 public class CreateS3PathServiceImpl implements CreateS3PathService {
 
-    private final S3Presigner s3Presigner;
+    private final S3Util s3Util;
 
     @Value("${s3.bucket.temp}")
     private String tempBucket;
 
     private static final String ALLOWED_TYPE = "image/png";
 
+    /**
+     * 파일 확장자 검증 (PNG만 허용)
+     * Presigned URL 생성
+     */
     @Override
     public S3PathResponse createS3Path(String contentType) {
 
         validateContentType(contentType);
 
-        String tempKey = "temp/"
+        String tempKey = generateTempKey();
+
+        String presignedUrl = s3Util.createPutPresignedUrl(
+                tempBucket,
+                tempKey,
+                ALLOWED_TYPE,
+                Duration.ofMinutes(5)
+        );
+
+        return new S3PathResponse(
+                presignedUrl,
+                tempKey
+        );
+    }
+
+    private String generateTempKey() {
+        return "temp/"
                 + LocalDate.now()
                 + "/"
                 + UUID.randomUUID()
                 + ".png";
-
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
-                .bucket(tempBucket)
-                .key(tempKey)
-                .contentType(ALLOWED_TYPE)
-                .build();
-
-        PutObjectPresignRequest presignRequest =
-                PutObjectPresignRequest.builder()
-                        .signatureDuration(Duration.ofMinutes(5))
-                        .putObjectRequest(objectRequest)
-                        .build();
-
-        PresignedPutObjectRequest presignedRequest =
-                s3Presigner.presignPutObject(presignRequest);
-
-        return new S3PathResponse(
-                presignedRequest.url().toString(),
-                tempKey
-        );
     }
 
     private void validateContentType(String contentType) {
