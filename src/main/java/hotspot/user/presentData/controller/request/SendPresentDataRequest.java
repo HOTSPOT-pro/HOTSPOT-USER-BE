ca7af6f6155f981c41255aa02f1d5d66c2c212cc
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-26 16:35:01 +0900

    [HOTSPOT-179] fix: 데이터 선물 request dto @NotNull 추가

--- src/main/java/hotspot/user/presentData/controller/request/SendPresentDataRequest.java
+++ src/main/java/hotspot/user/presentData/controller/request/SendPresentDataRequest.java
@@ -1,12 +1,14 @@
 package hotspot.user.presentData.controller.request;
 
+import jakarta.validation.constraints.NotNull;
+
 /**
  * 데이터 선물하기 request dto
  * @param targetSubId
  * @param dataAmount
  */
 public record SendPresentDataRequest(
-        Long targetSubId,
-        Long dataAmount
+        @NotNull Long targetSubId,
+        @NotNull Long dataAmount
 ) {
 }
