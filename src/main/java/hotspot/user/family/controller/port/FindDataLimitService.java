a7b42691b01142425ac4e014d9d2e059af60c784
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-26 22:22:59 +0900

    feat: 구성원 데이터 한도 및 차단 여부 조회 Service 생성

---
+++ src/main/java/hotspot/user/family/controller/port/FindDataLimitService.java
@@ -0,0 +1,13 @@
+package hotspot.user.family.controller.port;
+
+import hotspot.user.family.controller.response.FindDataLimitResponse;
+import hotspot.user.member.domain.FamilyRole;
+
+/**
+ * 특정 구성원의 데이터 한도 업데이트
+ */
+public interface FindDataLimitService {
+    FindDataLimitResponse findDataLimit(Long targetSubId,
+                                          Long requesterFamilyId,
+                                          FamilyRole requesterRole);
+}
