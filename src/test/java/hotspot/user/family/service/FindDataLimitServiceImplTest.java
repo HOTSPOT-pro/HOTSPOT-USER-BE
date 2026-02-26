8fcc409c344ff723951bdac2722aaf217db06e19
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-26 22:38:55 +0900

    refactor: spotlessApply & checkStyle 적용

--- src/test/java/hotspot/user/family/service/FindDataLimitServiceImplTest.java
+++ src/test/java/hotspot/user/family/service/FindDataLimitServiceImplTest.java
@@ -3,7 +3,6 @@ package hotspot.user.family.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
