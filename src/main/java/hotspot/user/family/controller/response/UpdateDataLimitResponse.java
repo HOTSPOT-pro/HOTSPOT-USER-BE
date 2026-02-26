80633334b6920243e11dd0b9b3166a8315720eea
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-27 02:31:16 +0900

    [HOTSPOT-181] fix: dataLimit dto에서 long -> int로 변경

--- src/main/java/hotspot/user/family/controller/response/UpdateDataLimitResponse.java
+++ src/main/java/hotspot/user/family/controller/response/UpdateDataLimitResponse.java
@@ -13,7 +13,7 @@ import lombok.Builder;
 public record UpdateDataLimitResponse(
         Long familyId,
         Long subId,
-        long dataLimit,
+        int dataLimit,
         boolean isLocked
 ) {
 }
