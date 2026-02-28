c616862ebc67e44e52764ebced1adc4023521f1a
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-01 01:35:18 +0900

    [HOTSPOT-185] fix: 정책 적용 repository에 deactive / active 메서드 추가

--- src/main/java/hotspot/user/policy/infrastructure/PolicySubJpaRepository.java
+++ src/main/java/hotspot/user/policy/infrastructure/PolicySubJpaRepository.java
@@ -15,5 +15,10 @@ public interface PolicySubJpaRepository extends JpaRepository<PolicySubEntity, L
     // N+1 SELECT를 방지하기 위한 벌크 UPDATE 쿼리 (비활성화, isActive = false)
     @Modifying(clearAutomatically = true)
     @Query("UPDATE PolicySubEntity p SET p.isActive = false WHERE p.policySubId IN :ids")
-    void bulkSoftDelete(@Param("ids") List<Long> ids);
+    void bulkDeActive(@Param("ids") List<Long> ids);
+
+    // N+1 SELECT를 방지하기 위한 벌크 UPDATE 쿼리 (활성화, isActive = true)
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE PolicySubEntity p SET p.isActive = true WHERE p.policySubId IN :ids")
+    void bulkActivate(@Param("ids") List<Long> ids);
 }
--- src/main/java/hotspot/user/policy/infrastructure/PolicySubRepositoryImpl.java
+++ src/main/java/hotspot/user/policy/infrastructure/PolicySubRepositoryImpl.java
@@ -24,18 +24,21 @@ public class PolicySubRepositoryImpl implements PolicySubRepository {
 
 
     // 정책-회선 매핑 리스트 저장
-    // 새로운 INSERT, 기존 isActive=false 분리해서 진행 (N+1 방지)
+    // 새로운 INSERT, 기존 isActive 업데이트 분리해서 진행 (N+1 방지)
     @Override
     public List<PolicySub> saveAll(List<PolicySub> policySubList) {
         List<PolicySubEntity> entitiesToInsert = new ArrayList<>();
-        List<Long> idsToUpdate = new ArrayList<>();
+        List<Long> idsToDeactivate = new ArrayList<>();
+        List<Long> idsToActivate = new ArrayList<>();
 
-        // 단 한 번의 순회로 Insert 대상과 Update(Delete) 대상을 분류
+        // 단 한 번의 순회로 Insert 대상과 Update(Delete/Activate) 대상을 분류
         policySubList.forEach(domain -> {
             if (domain.getId() == null) {
                 entitiesToInsert.add(PolicySubEntity.domainToEntity(domain));
             } else if (!domain.isActive()) {
-                idsToUpdate.add(domain.getId());
+                idsToDeactivate.add(domain.getId());
+            } else {
+                idsToActivate.add(domain.getId());
             }
         });
 
@@ -46,9 +49,14 @@ public class PolicySubRepositoryImpl implements PolicySubRepository {
             savedEntities = policySubJpaRepository.saveAll(entitiesToInsert);
         }
 
-        // 2. 벌크 Soft Delete 실행 (isActive = false로 변경)
-        if (!idsToUpdate.isEmpty()) {
-            policySubJpaRepository.bulkSoftDelete(idsToUpdate);
+        // 2. 벌크 비활성화 실행 (isActive = false로 변경)
+        if (!idsToDeactivate.isEmpty()) {
+            policySubJpaRepository.bulkDeActive(idsToDeactivate);
+        }
+
+        // 3. 벌크 활성화 실행 (isActive = true로 변경)
+        if (!idsToActivate.isEmpty()) {
+            policySubJpaRepository.bulkActivate(idsToActivate);
         }
 
         return savedEntities.stream()
