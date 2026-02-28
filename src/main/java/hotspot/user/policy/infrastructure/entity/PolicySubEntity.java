fc748ea63e2e690007cfc67c994176ad2835f3d1
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-01 03:22:53 +0900

    [HOTSPOT-185] refactor : spotlessApply & checkstylemain 적용

--- src/main/java/hotspot/user/policy/infrastructure/entity/PolicySubEntity.java
+++ src/main/java/hotspot/user/policy/infrastructure/entity/PolicySubEntity.java
@@ -1,6 +1,15 @@
 package hotspot.user.policy.infrastructure.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import hotspot.user.common.BaseEntity;
 import hotspot.user.policy.domain.PolicySub;
