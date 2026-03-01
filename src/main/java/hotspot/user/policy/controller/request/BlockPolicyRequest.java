6090afb124971dcafbc39f7fa6b590da2703f0a3
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-02 00:29:01 +0900

    [HOTSPOT-201] fix: 정책 생성 dto & mapper 수정

--- src/main/java/hotspot/user/policy/controller/request/BlockPolicyRequest.java
+++ src/main/java/hotspot/user/policy/controller/request/BlockPolicyRequest.java
@@ -2,15 +2,21 @@ package hotspot.user.policy.controller.request;
 
 import hotspot.user.policy.domain.PolicySnapshot;
 import hotspot.user.policy.domain.PolicyType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 /**
  * 정책 생성 / 수정 request dto
  */
 public record BlockPolicyRequest(
         Long id,
+        @NotBlank(message = "정책 이름은 필수입니다.")
         String name,
+        @NotNull(message = "정책 타입은 필수입니다.")
         PolicyType policyType,
+        @NotNull(message = "정책 스냅샷은 필수입니다.")
         PolicySnapshot policySnapshot,
+        @NotBlank(message = "정책 설명은 필수입니다.")
         String policyDescription,
         boolean isActive
 ) {
