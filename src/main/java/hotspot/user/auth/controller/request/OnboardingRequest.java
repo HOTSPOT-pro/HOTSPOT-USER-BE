65bf9bc78190e4554763c459d4c68c6ac1f2dd9c
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-02 17:11:55 +0900

    [HOTSPOT-182] fix: 전화번호 정규식 수정

--- src/main/java/hotspot/user/auth/controller/request/OnboardingRequest.java
+++ src/main/java/hotspot/user/auth/controller/request/OnboardingRequest.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.Pattern;
  */
 public record OnboardingRequest(
         @NotBlank(message = "전화번호는 필수입니다.")
-        @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$", message = "올바른 전화번호 형식이 아닙니다.")
         String phoneNumber,
 
         @NotBlank(message = "생년월일은 필수입니다.")
