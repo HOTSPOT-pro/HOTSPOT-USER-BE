45c586417a789fccae68b24151d712750a50c1b3
Jae <lijae3552@gmail.com>
2026-03-03 21:44:21 +0900

    [HOTSPOT-219] refactor: 기존 dispatch 패키지의 SSE 구독 포트 제거

--- src/main/java/hotspot/user/dispatch/controller/port/SubscribeSseService.java
+++
@@ -1,8 +0,0 @@
-package hotspot.user.dispatch.controller.port;
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-public interface SubscribeSseService {
-
-    SseEmitter subscribe(Long memberId, String lastEventId);
-}
