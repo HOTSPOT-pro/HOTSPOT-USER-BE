0817bd2e22e97994d233113ea07bd024af529111
andrew <leegd120@gmail.com>
2026-03-03 18:54:29 +0900

    fix : PresignedURL 생성시 확장자 검증 로직 제거

--- src/main/java/hotspot/user/s3/controller/port/CreateS3PathService.java
+++ src/main/java/hotspot/user/s3/controller/port/CreateS3PathService.java
@@ -4,5 +4,5 @@ import hotspot.user.s3.controller.response.S3PathResponse;
 
 public interface CreateS3PathService {
 
-    S3PathResponse createS3Path(String contentType);
+    S3PathResponse createS3Path();
 }
