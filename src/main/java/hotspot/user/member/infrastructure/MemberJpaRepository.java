53aacf1e56578542692e5f9eb6ea51cb487adc27
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-06 00:35:31 +0900

    [HOTSPOT-262] fix: MemberJpaRepository isDeleted = false 조건 추가

--- src/main/java/hotspot/user/member/infrastructure/MemberJpaRepository.java
+++ src/main/java/hotspot/user/member/infrastructure/MemberJpaRepository.java
@@ -18,19 +18,21 @@ public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
     // MemberDetailInfoDto로 바로 변환해야함 But, Builder는 JPQL에서 불가능
     // [To-Do] 1:N 관계(다중 회선) 발생 시 중복 행 문제 해결 필요
     // - Subscription: 현재는 1:1로 가정하나, 다중 회선 확장 시 최신/대표 회선 필터링 조건 추가 필요
-    @Query("""
-           SELECT new hotspot.user.member.infrastructure.entity.MemberDetailInfoDto(
-               m, :email, s.phoneEnc, s.subId, fs.familyRole, fs.family.familyId
-           )
-           FROM MemberEntity m
-           LEFT JOIN SubscriptionEntity s ON s.member = m
-                AND s.subId = (SELECT MAX(s2.subId) FROM SubscriptionEntity s2 WHERE s2.member = m)
-           LEFT JOIN FamilySubscriptionEntity fs ON fs.subscription = s
-           WHERE m.id = :memberId
-             AND EXISTS (
-                 SELECT 1 FROM SocialAccountEntity sa
-                 WHERE sa.member = m AND sa.email = :email
-             )
-           """)
-    Optional<MemberDetailInfoDto> findDetailQueryResult(@Param("memberId") Long memberId, @Param("email") String email);
-}
+            @Query("""
+                   SELECT new hotspot.user.member.infrastructure.entity.MemberDetailInfoDto(
+                       m, :email, s.phoneEnc, s.subId, fs.familyRole, fs.family.familyId
+                   )
+                   FROM MemberEntity m
+                   LEFT JOIN SubscriptionEntity s ON s.member = m 
+                        AND s.subId = (SELECT MAX(s2.subId) FROM SubscriptionEntity s2 WHERE s2.member = m)
+                   LEFT JOIN FamilySubscriptionEntity fs ON fs.subscription = s
+                   WHERE m.id = :memberId
+                     AND m.isDeleted = false
+                     AND (:email IS NULL OR EXISTS (
+                         SELECT 1 FROM SocialAccountEntity sa
+                         WHERE sa.member = m AND sa.email = :email
+                     ))
+                   """)
+            Optional<MemberDetailInfoDto> findDetailQueryResult(@Param("memberId") Long memberId, @Param("email") String email);
+        }
+        
