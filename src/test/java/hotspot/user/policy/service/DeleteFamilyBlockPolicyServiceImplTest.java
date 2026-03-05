a1556a7209d49cd9cc51871b4a1b6232ea6fb944
andrew <leegd120@gmail.com>
2026-03-06 03:07:35 +0900

    test : Test 코드 작성

--- src/test/java/hotspot/user/policy/service/DeleteFamilyBlockPolicyServiceImplTest.java
+++ src/test/java/hotspot/user/policy/service/DeleteFamilyBlockPolicyServiceImplTest.java
@@ -27,6 +27,7 @@ import hotspot.user.member.service.port.MemberRepository;
 import hotspot.user.policy.domain.BlockPolicy;
 import hotspot.user.policy.service.port.BlockPolicyRepository;
 import hotspot.user.policy.service.port.PolicySubRepository;
+import hotspot.user.policy.service.util.PolicyDeletedOutboxPublisher;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteFamilyBlockPolicyServiceImplTest {
@@ -40,6 +41,9 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Mock
     private PolicySubRepository policySubRepository;
 
+    @Mock
+    private PolicyDeletedOutboxPublisher policyDeletedOutboxPublisher;
+
     @InjectMocks
     private DeleteFamilyBlockPolicyServiceImpl deleteFamilyBlockPolicyService;
 
@@ -49,8 +53,9 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Test
     @DisplayName("성공: 우리 가족 정책을 일괄 삭제한다")
     void deleteSuccess() {
-        // given
+
         List<Long> policyIds = List.of(10L, 20L);
+
         MemberDetailInfo memberDetail = MemberDetailInfo.builder()
                 .familyId(FAMILY_ID)
                 .role(FamilyRole.OWNER)
@@ -59,14 +64,26 @@ class DeleteFamilyBlockPolicyServiceImplTest {
         given(memberRepository.findDetailByIdAndEmail(anyLong(), isNull()))
                 .willReturn(Optional.of(memberDetail));
 
-        BlockPolicy policy1 = BlockPolicy.builder().id(10L).familyId(FAMILY_ID).build();
-        BlockPolicy policy2 = BlockPolicy.builder().id(20L).familyId(FAMILY_ID).build();
-        given(blockPolicyRepository.findAllById(policyIds)).willReturn(List.of(policy1, policy2));
+        BlockPolicy policy1 = BlockPolicy.builder()
+                .id(10L)
+                .familyId(FAMILY_ID)
+                .build();
+
+        BlockPolicy policy2 = BlockPolicy.builder()
+                .id(20L)
+                .familyId(FAMILY_ID)
+                .build();
+
+        given(blockPolicyRepository.findAllById(policyIds))
+                .willReturn(List.of(policy1, policy2));
 
         // when
         deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID);
 
         // then
+        verify(policyDeletedOutboxPublisher).publish(10L, FAMILY_ID);
+        verify(policyDeletedOutboxPublisher).publish(20L, FAMILY_ID);
+
         verify(policySubRepository).bulkDeActiveByBlockPolicyIds(policyIds);
         verify(blockPolicyRepository).bulkDelete(policyIds);
     }
@@ -74,8 +91,9 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Test
     @DisplayName("실패: 요청자가 가족의 OWNER가 아니면 예외가 발생한다")
     void deleteFailNotOwner() {
-        // given
+
         List<Long> policyIds = List.of(10L);
+
         MemberDetailInfo memberDetail = MemberDetailInfo.builder()
                 .familyId(FAMILY_ID)
                 .role(FamilyRole.PARENT)
@@ -84,8 +102,9 @@ class DeleteFamilyBlockPolicyServiceImplTest {
         given(memberRepository.findDetailByIdAndEmail(anyLong(), isNull()))
                 .willReturn(Optional.of(memberDetail));
 
-        // when & then
-        assertThatThrownBy(() -> deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID))
+        assertThatThrownBy(() ->
+                deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID)
+        )
                 .isInstanceOf(ApplicationException.class)
                 .hasFieldOrPropertyWithValue("code", AuthErrorCode.ACCESS_DENIED);
     }
@@ -93,8 +112,9 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Test
     @DisplayName("실패: 요청된 정책 ID 중 존재하지 않는 ID가 있으면 예외가 발생한다")
     void deleteFailPolicyNotFound() {
-        // given
+
         List<Long> policyIds = List.of(10L, 999L);
+
         MemberDetailInfo memberDetail = MemberDetailInfo.builder()
                 .familyId(FAMILY_ID)
                 .role(FamilyRole.OWNER)
@@ -103,12 +123,17 @@ class DeleteFamilyBlockPolicyServiceImplTest {
         given(memberRepository.findDetailByIdAndEmail(anyLong(), isNull()))
                 .willReturn(Optional.of(memberDetail));
 
-        // 10L은 존재하지만 999L은 DB에 없음
-        BlockPolicy policy1 = BlockPolicy.builder().id(10L).familyId(FAMILY_ID).build();
-        given(blockPolicyRepository.findAllById(policyIds)).willReturn(List.of(policy1));
+        BlockPolicy policy1 = BlockPolicy.builder()
+                .id(10L)
+                .familyId(FAMILY_ID)
+                .build();
 
-        // when & then
-        assertThatThrownBy(() -> deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID))
+        given(blockPolicyRepository.findAllById(policyIds))
+                .willReturn(List.of(policy1));
+
+        assertThatThrownBy(() ->
+                deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID)
+        )
                 .isInstanceOf(ApplicationException.class)
                 .hasFieldOrPropertyWithValue("code", PolicyErrorCode.POLICY_NOT_FOUND);
     }
@@ -116,8 +141,9 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Test
     @DisplayName("실패: 요청된 정책 중 타인의 가족 정책이 포함되어 있으면 예외가 발생한다")
     void deleteFailForeignPolicy() {
-        // given
+
         List<Long> policyIds = List.of(10L, 50L);
+
         MemberDetailInfo memberDetail = MemberDetailInfo.builder()
                 .familyId(FAMILY_ID)
                 .role(FamilyRole.OWNER)
@@ -126,12 +152,22 @@ class DeleteFamilyBlockPolicyServiceImplTest {
         given(memberRepository.findDetailByIdAndEmail(anyLong(), isNull()))
                 .willReturn(Optional.of(memberDetail));
 
-        BlockPolicy policy1 = BlockPolicy.builder().id(10L).familyId(FAMILY_ID).build();
-        BlockPolicy policy2 = BlockPolicy.builder().id(50L).familyId(200L).build(); // 타인 가족
-        given(blockPolicyRepository.findAllById(policyIds)).willReturn(List.of(policy1, policy2));
+        BlockPolicy policy1 = BlockPolicy.builder()
+                .id(10L)
+                .familyId(FAMILY_ID)
+                .build();
 
-        // when & then
-        assertThatThrownBy(() -> deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID))
+        BlockPolicy policy2 = BlockPolicy.builder()
+                .id(50L)
+                .familyId(200L)
+                .build();
+
+        given(blockPolicyRepository.findAllById(policyIds))
+                .willReturn(List.of(policy1, policy2));
+
+        assertThatThrownBy(() ->
+                deleteFamilyBlockPolicyService.delete(policyIds, MEMBER_ID, FAMILY_ID)
+        )
                 .isInstanceOf(ApplicationException.class)
                 .hasFieldOrPropertyWithValue("code", PolicyErrorCode.POLICY_ACCESS_DENIED);
     }
@@ -139,12 +175,13 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Test
     @DisplayName("실패: 회원을 찾을 수 없으면 예외가 발생한다")
     void deleteFailMemberNotFound() {
-        // given
+
         given(memberRepository.findDetailByIdAndEmail(anyLong(), isNull()))
                 .willReturn(Optional.empty());
 
-        // when & then
-        assertThatThrownBy(() -> deleteFamilyBlockPolicyService.delete(List.of(1L), MEMBER_ID, FAMILY_ID))
+        assertThatThrownBy(() ->
+                deleteFamilyBlockPolicyService.delete(List.of(1L), MEMBER_ID, FAMILY_ID)
+        )
                 .isInstanceOf(ApplicationException.class)
                 .hasFieldOrPropertyWithValue("code", MemberErrorCode.MEMBER_NOT_FOUND);
     }
@@ -152,17 +189,18 @@ class DeleteFamilyBlockPolicyServiceImplTest {
     @Test
     @DisplayName("실패: 요청자의 가족 ID와 회원의 소속 가족 ID가 다르면 예외가 발생한다")
     void deleteFailDifferentFamily() {
-        // given
+
         MemberDetailInfo memberDetail = MemberDetailInfo.builder()
-                .familyId(200L) // 다른 가족
+                .familyId(200L)
                 .role(FamilyRole.OWNER)
                 .build();
 
         given(memberRepository.findDetailByIdAndEmail(anyLong(), isNull()))
                 .willReturn(Optional.of(memberDetail));
 
-        // when & then
-        assertThatThrownBy(() -> deleteFamilyBlockPolicyService.delete(List.of(1L), MEMBER_ID, FAMILY_ID))
+        assertThatThrownBy(() ->
+                deleteFamilyBlockPolicyService.delete(List.of(1L), MEMBER_ID, FAMILY_ID)
+        )
                 .isInstanceOf(ApplicationException.class)
                 .hasFieldOrPropertyWithValue("code", FamilyErrorCode.NOT_FAMILY_MEMBER);
     }
