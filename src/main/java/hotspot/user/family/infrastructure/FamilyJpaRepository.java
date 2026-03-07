034ba0b8bcb86b0124e4b5d9ed350eada9abeb6c
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-07 17:06:20 +0900

    [HOTSPOT-262] fix: FamilyJpaRepository SocialAccount id 내림차순으로 조회하도록 수정

--- src/main/java/hotspot/user/family/infrastructure/FamilyJpaRepository.java
+++ src/main/java/hotspot/user/family/infrastructure/FamilyJpaRepository.java
@@ -14,19 +14,21 @@ import hotspot.user.family.infrastructure.entity.FamilyEntity;
  */
 public interface FamilyJpaRepository extends JpaRepository<FamilyEntity, Long> {
 
-    @Query("""
-           SELECT DISTINCT new hotspot.user.family.infrastructure.entity.FamilyDetailInfoDto(
-               f, m, (
-                   SELECT MAX(sa.email) FROM SocialAccountEntity sa
-                   WHERE sa.member = m AND sa.isDeleted = false
-               ), s.phoneEnc, s.subId, fs.familyRole
-           )
-           FROM FamilySubscriptionEntity fs
-           JOIN fs.family f
-           JOIN fs.subscription s
-           JOIN s.member m
-           WHERE f.familyId = :familyId
-           """)
-    List<FamilyDetailInfoDto> findFamilyDetailQueryResult(
-            @Param("familyId") Long familyId);
+        @Query("""
+               SELECT DISTINCT new hotspot.user.family.infrastructure.entity.FamilyDetailInfoDto(
+                   f, m, (
+                       SELECT sa.email FROM SocialAccountEntity sa 
+                       WHERE sa.id = (
+                           SELECT MAX(sa2.id) FROM SocialAccountEntity sa2 
+                           WHERE sa2.member = m AND sa2.isDeleted = false
+                       )
+                   ), s.phoneEnc, s.subId, fs.familyRole
+               )
+               FROM FamilySubscriptionEntity fs
+               JOIN fs.family f
+               JOIN fs.subscription s
+               JOIN s.member m
+               WHERE f.familyId = :familyId
+               """)
+        List<FamilyDetailInfoDto> findFamilyDetailQueryResult(            @Param("familyId") Long familyId);
 }
