b8ecb012295774f4dc2f297e037701d20e635869
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-07 17:13:11 +0900

    [HOTSPOT-262] refactor: spotlessApply 적용

--- src/main/java/hotspot/user/family/infrastructure/FamilyJpaRepository.java
+++ src/main/java/hotspot/user/family/infrastructure/FamilyJpaRepository.java
@@ -17,9 +17,9 @@ public interface FamilyJpaRepository extends JpaRepository<FamilyEntity, Long> {
         @Query("""
                SELECT DISTINCT new hotspot.user.family.infrastructure.entity.FamilyDetailInfoDto(
                    f, m, (
-                       SELECT sa.email FROM SocialAccountEntity sa 
+                       SELECT sa.email FROM SocialAccountEntity sa
                        WHERE sa.id = (
-                           SELECT MAX(sa2.id) FROM SocialAccountEntity sa2 
+                           SELECT MAX(sa2.id) FROM SocialAccountEntity sa2
                            WHERE sa2.member = m AND sa2.isDeleted = false
                        )
                    ), s.phoneEnc, s.subId, fs.familyRole
