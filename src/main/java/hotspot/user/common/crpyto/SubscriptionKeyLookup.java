1000de8b8135405743b17c3389e9fd71cf28900b
arkchive <kyungminkim7@konkuk.ac.kr>
2026-03-10 16:39:38 +0900

    [HOTSPOT-293] spotlessApply 적용

--- src/main/java/hotspot/user/common/crpyto/SubscriptionKeyLookup.java
+++ src/main/java/hotspot/user/common/crpyto/SubscriptionKeyLookup.java
@@ -7,9 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
