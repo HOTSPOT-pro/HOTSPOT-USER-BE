eb06b4fa55e1b1cdc89377c6691c707023ae9c31
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-11 11:20:50 +0900

    [HOTSPOT-306] fix: validateEnoughPlanRemaining 로직 수정

--- src/main/java/hotspot/user/presentData/service/SendPresentDataServiceImpl.java
+++ src/main/java/hotspot/user/presentData/service/SendPresentDataServiceImpl.java
@@ -105,10 +105,9 @@ public class SendPresentDataServiceImpl implements SendPresentDataService {
 
         DataPeriod period = giverSubscription.getPlan().getDataPeriod();
 
-        long dataAmount = giverSubscription.getPlan().getDataAmount();
-
-        // 무제한 용금제일 경우에는 무조건 선물 가능하도록 예외 처리 로직 추가
-        if(dataAmount <= -1) return;
+        Long dataAmount = giverSubscription.getPlan().getDataAmount();
+        // 무제한 요금제일 경우에는 무조건 선물 가능하도록 예외 처리 로직 추가
+        if (dataAmount != null && dataAmount == -1L) return;
 
         long remainingKb =
                 subscriptionUsageRepository.findRemainingPlanKb(giverSubscription.getId(), period);
