d7f765b3fbdf7dd6066f743108ce874ed3619c6f
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-11 22:21:21 +0900

    [HOTSPOT-320] chore: WebConfig 원상 복구 및 resolver 파일 삭제

--- src/main/java/hotspot/user/common/config/WebConfig.java
+++ src/main/java/hotspot/user/common/config/WebConfig.java
@@ -1,12 +1,8 @@
 package hotspot.user.common.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import hotspot.user.common.security.resolver.CurrentFamilyIdResolver;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -15,15 +11,4 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    private final CurrentFamilyIdResolver currentFamilyIdResolver;
-
-    /**
-     * 커스텀 ArgumentResolver 등록
-     * @param resolvers 등록할 Resolver 리스트
-     */
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(currentFamilyIdResolver);
-    }
 }
