70a7c69af7c5547f01c47d665ec9b93d4c19080a
arkchive <kyungminkim7@konkuk.ac.kr>
2026-03-15 18:40:32 +0900

    [HOTSPOT-350] docs: 리포트 분석 서비스 구독 여부 조회 스웨거 추가

---
+++ src/main/java/hotspot/user/familyReport/controller/swagger/FamilyReportApi.java
@@ -0,0 +1,29 @@
+package hotspot.user.familyReport.controller.swagger;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import hotspot.user.common.ApiResponse;
+import hotspot.user.common.exception.ErrorResponse;
+import hotspot.user.common.security.PrincipalDetails;
+import hotspot.user.familyReport.controller.response.FamilyReportSubscriptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "AI Report", description = "AI 주간 리포트 구독 API")
+public interface FamilyReportApi {
+
+    @Operation(summary = "가족 AI 리포트 구독 여부 조회",
+            description = "현재 로그인한 사용자가 속한 가족의 AI 주간 리포트 구독 여부와 수신 요일을 조회합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "조회 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<ApiResponse<FamilyReportSubscriptionResponse>> findFamilyReportSubscription(
+            @Parameter(hidden = true) @AuthenticationPrincipal PrincipalDetails principal);
+}
