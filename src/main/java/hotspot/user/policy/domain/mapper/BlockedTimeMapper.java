75a0efcc6f769785550af6287549ab2474b57386
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-16 19:44:39 +0900

    [HOTSPOT-359] feat: BlockTime 도메인, mapper 생성

---
+++ src/main/java/hotspot/user/policy/domain/mapper/BlockedTimeMapper.java
@@ -0,0 +1,43 @@
+package hotspot.user.policy.domain.mapper;
+
+import java.time.DayOfWeek;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import hotspot.user.policy.controller.response.AppliedPolicyResponse;
+import hotspot.user.policy.controller.response.BlockedRangeResponse;
+import hotspot.user.policy.controller.response.BlockedTimeResponse;
+import hotspot.user.policy.controller.response.DayBlockedTimeResponse;
+import hotspot.user.policy.domain.BlockedTime;
+
+/**
+ * 차단 시간대 정보를 DTO로 변환하는 매퍼
+ */
+public class BlockedTimeMapper {
+
+    /**
+     * BlockedTime 도메인 객체를 BlockedTimeResponse DTO로 변환
+     */
+    public static BlockedTimeResponse toBlockedTimeResponse(
+            AppliedPolicyResponse policyResponse,
+            BlockedTime blockedTime
+    ) {
+        List<DayBlockedTimeResponse> dayBlockedTimes = blockedTime.getDayRanges().entrySet().stream()
+                .map(entry -> DayBlockedTimeResponse.builder()
+                        .day(entry.getKey())
+                        .ranges(entry.getValue().stream()
+                                .map(range -> BlockedRangeResponse.builder()
+                                        .startTime(range.start())
+                                        .endTime(range.end())
+                                        .build())
+                                .toList())
+                        .build())
+                .sorted((o1, o2) -> o1.day().compareTo(o2.day()))
+                .collect(Collectors.toList());
+
+        return BlockedTimeResponse.builder()
+                .subId(policyResponse.subId())
+                .dayBlockedTimes(dayBlockedTimes)
+                .build();
+    }
+}
