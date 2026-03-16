271c3080a001efd8aa508923fe1ed6a8ab079b83
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-16 14:56:23 +0900

    [HOTSPOT-355] chore: checkstyle 적용

--- src/main/java/hotspot/user/weeklyReport/infrastructure/WeeklyReportJpaRepository.java
+++ src/main/java/hotspot/user/weeklyReport/infrastructure/WeeklyReportJpaRepository.java
@@ -1,7 +1,5 @@
 package hotspot.user.weeklyReport.infrastructure;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hotspot.user.weeklyReport.infrastructure.entity.WeeklyReportEntity;
