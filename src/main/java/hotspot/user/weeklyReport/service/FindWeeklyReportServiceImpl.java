0e2f5402b41ef22effdeac490c47258049fce198
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-16 10:30:19 +0900

    [HOTSPOT-355] fix: WeeklyReport 에러 코드 추가 및 service 코드 수정

--- src/main/java/hotspot/user/weeklyReport/service/FindWeeklyReportServiceImpl.java
+++ src/main/java/hotspot/user/weeklyReport/service/FindWeeklyReportServiceImpl.java
@@ -1,30 +1,30 @@
 package hotspot.user.weeklyReport.service;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import hotspot.user.weeklyReport.service.port.WeeklyReportRepository;
+import hotspot.user.weeklyReport.domain.mapper.WeeklyReportMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import hotspot.user.common.exception.ApplicationException;
+import hotspot.user.common.exception.code.WeeklyReportErrorCode;
 import hotspot.user.weeklyReport.controller.port.FindWeeklyReportService;
 import hotspot.user.weeklyReport.controller.response.WeeklyReportResponse;
-import hotspot.user.weeklyReport.domain.AIFeedback;
-import hotspot.user.weeklyReport.domain.ScoreData;
-import hotspot.user.weeklyReport.domain.SummaryData;
-import hotspot.user.weeklyReport.domain.UsageListData;
+import hotspot.user.weeklyReport.domain.WeeklyReport;
+import hotspot.user.weeklyReport.service.port.WeeklyReportRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class FindWeeklyReportServiceImpl implements FindWeeklyReportService {
+
     private final WeeklyReportRepository weeklyReportRepository;
 
     @Override
     public WeeklyReportResponse findWeeklyReport(Long memberId, Long subId, Long reportId) {
-        return WeeklyReportResponse.builder().build();
+        WeeklyReport report = weeklyReportRepository.findByReportId(reportId)
+                .orElseThrow(() -> new ApplicationException(WeeklyReportErrorCode.REPORT_NOT_FOUND));
+
+        return WeeklyReportMapper.toWeeklyReportResponse(report);
     }
+
 }
